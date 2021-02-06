749c0acdaa1584aa2f254c72ee81e7d65e8864d7
Ana Silvia Nazar Luna <ananazar@hotmail.com>
2021-02-06 17:26:41 -0300

    feat: Criação da página Landing

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import { Container, SectionFull } from './styles';
+
 const Landing: React.FC = () => (
-  <>
-    <h1>Landing</h1>
-  </>
+  <Container>
+    <header>
+      <div>Logo</div>
+      <nav>Para Candidatos Para Empresas</nav>
+      <button type="button">Login</button>
+    </header>
+
+    <main>
+      <SectionFull>
+        <h1>Landing</h1>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit similique
+          tempore eum mollitia tempora laborum debitis adipisci.
+        </h2>
+        <button type="button">Candidatos</button>
+        <button type="button">Empresas</button>
+      </SectionFull>
+    </main>
+
+    <footer>
+      Feito por mulheres incríves ❤
+      <br />
+      2021. Todos os direitos reservados.
+      <br />
+      redes sociais
+    </footer>
+  </Container>
 );
 
 export default Landing;
